479e0e171352a97391f44e0680c8b10a91eaec17
mayerprog <mairuhin@mail.ru>
2024-02-18 19:27:46 +0300

    style: fix cart containers

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,9 +2,11 @@ import styles from "./Cart.module.scss";
 
 const Cart = () => {
   return (
-    <div className={styles.cartBox}>
-      <span className={styles.cartTitle}>Корзина</span>
-      <span className={styles.deleteTitle}>Очистить</span>
+    <div className={styles.cart}>
+      <div className={styles.cartBox}>
+        <span className={styles.cartTitle}>Корзина</span>
+        <span className={styles.deleteTitle}>Очистить</span>
+      </div>
     </div>
   );
 };
