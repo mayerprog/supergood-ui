ccddb924772928423b9662bef23ab563ebdbb285
mayerprog <mairuhin@mail.ru>
2024-02-21 00:06:07 +0300

    style: change min-width of parent

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,10 +3,11 @@ import styles from "./Cart.module.scss";
 const Cart = () => {
   return (
     <div className={styles.cart}>
-      <div className={styles.cartBox}>
+      <div className={styles.cartHeader}>
         <span className={styles.cartTitle}>Корзина</span>
         <span className={styles.deleteTitle}>Очистить</span>
       </div>
+      <div className={styles.cartBox}></div>
     </div>
   );
 };
