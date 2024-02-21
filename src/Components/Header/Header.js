ddcea218b5a699df6917e949a3e25ff76118ec6b
mayerprog <mairuhin@mail.ru>
2024-02-21 17:07:05 +0300

    style: add icons to header components

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import styles from "./Header.module.scss";
 import logo from "../../assets/images/logo.jpg";
 import socialNet from "../../assets/images/socialnetworks.jpg";
 import { GiShoppingCart } from "react-icons/gi";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
       <img src={logo} alt="" className={styles.logo} />
       <input className={styles.input} placeholder="Найти блюдо" />
       <button onClick={() => console.log("click")} className={styles.address}>
+        <FaLocationDot size={18} color="#BBBBBB" className={styles.icon} />
         <span className={styles.buttonText}>
           Большая Новодмитровская улица, 12с15
         </span>
