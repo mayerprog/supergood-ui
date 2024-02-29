689ee429dd9f9e3828408491749d791527c29b52
Майра Тулегенова <mayerprog@Majras-MacBook-Air.local>
2024-02-29 10:16:41 +0300

    fix sidebar styles

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,37 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import styles from "./Sidebar.module.scss";
 
 const Sidebar = ({ categories }) => {
-  const sidebarRef = useRef(null);
-  const [isSticky, setIsSticky] = useState(false);
-
-  // const handleScroll = () => {
-  //   if (!sidebarRef.current) return;
-
-  //   const sidebar = sidebarRef.current;
-  //   const sidebarBottom = sidebar.offsetTop + sidebar.offsetHeight;
-  //   const windowHeight = window.innerHeight;
-  //   const scrollY = window.scrollY;
-
-  //   if (sidebarBottom <= scrollY + windowHeight) {
-  //     setIsSticky(true);
-  //   } else {
-  //     setIsSticky(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   console.log();
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
-  // const sidebarClass = isSticky ? styles.sticky : styles.sidebar;
-
   return (
-    <div className={styles.sidebar} ref={sidebarRef}>
+    <div className={styles.sidebar}>
       {categories.map((item, index) => (
         <button key={index} className={styles.item}>
           {item}
