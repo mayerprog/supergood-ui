106e90ec9272c287ea3adfeda85587b98e6b357f
mayerprog <supermayerehs@gmail.com>
2024-03-17 22:38:02 +0300

    style: edit styles of footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import styles from "./Footer.module.scss";
+import AppStore from "../../assets/images/App-Store.png";
+import GooglePlay from "../../assets/images/Google-Play.png";
 import logo from "../../assets/images/logo.jpg";
 import { useNavigate } from "react-router-dom";
 
@@ -15,19 +17,65 @@ const Footer = () => {
       />
       <div className={styles.infoContainer}>
         <div className={styles.info}>
-          <span>О компании</span>
-          <span>Оплата и доставка</span>
-          <span>Сделать заказ</span>
+          <a
+            href="https://supergood.ru/about"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>О компании</span>
+          </a>
+          <a
+            href="https://supergood.ru/delivery-n-pay"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Оплата и доставка</span>
+          </a>
+          <a
+            href="https://supergood.ru/howto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Сделать заказ</span>
+          </a>
         </div>
         <div className={styles.info}>
-          <span>Акции и скидки</span>
-          <span>Контакты</span>
+          <a
+            href="https://supergood.ru/akcii"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Акции и скидки</span>
+          </a>
+          <a
+            href="https://supergood.ru/contacts"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Контакты</span>
+          </a>
         </div>
       </div>
-      <p>Author: Hege Refsnes</p>
-      <p>
-        <a href="mailto:hege@example.com">hege@example.com</a>
-      </p>
+      <div className={styles.appLinks}>
+        <span>С приложением удобнее!</span>
+        <div className={styles.images}>
+          <a
+            href="https://apps.apple.com/ru/app/supergood/id1520374517"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={AppStore} alt="" />
+          </a>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.psw.supergood"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={GooglePlay} alt="" />
+          </a>
+        </div>
+      </div>
+      <h2>+7 (495) 138 0123</h2>
     </footer>
   );
 };
