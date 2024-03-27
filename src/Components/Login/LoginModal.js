d0959cebe0da4fd863fba4d7e5831c19f5bd6e60
mayerprog <supermayerehs@gmail.com>
2024-03-27 17:25:35 +0300

    feat: addinput validation

--- src/Components/Login/LoginModal.js
+++ src/Components/Login/LoginModal.js
@@ -4,18 +4,29 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { setIsAuth } from "../../redux/slices/authSlice";
 import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const LoginModal = ({ loginWrapperRef, toggleLoginVisibility }) => {
   const [phone, setPhone] = useState("");
   const [code, setCode] = useState("");
   const [checked, setChecked] = useState(false);
   const [codeSent, setCodeSent] = useState(false);
+  const [isPhoneComplete, setIsPhoneComplete] = useState(false);
+  const [borderColor, setBorderColor] = useState("#ccc");
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const dynamicStyle = {
+    "--border-color": borderColor,
+  };
 
   const handleChangePhone = (event) => {
     let val = event.target.value;
     setPhone(val);
+    if (val.length < 18) setBorderColor("#c36060");
+    else setBorderColor("#ccc");
+    setIsPhoneComplete(val.length === 18);
   };
   const handleEnterCode = (event) => {
     let val = event.target.value;
@@ -33,7 +44,7 @@ const LoginModal = ({ loginWrapperRef, toggleLoginVisibility }) => {
     }
   };
   const handleLogin = () => {
-    if (checked && phone) setCodeSent(true);
+    if (checked && isPhoneComplete) setCodeSent(true);
   };
 
   return (
@@ -45,10 +56,10 @@ const LoginModal = ({ loginWrapperRef, toggleLoginVisibility }) => {
             mask="+7 (999) 999-99-99"
             maskChar={null}
             value={phone}
-            defaultValue="+7 "
             className={styles.input}
             placeholder="Введите телефон"
             onChange={handleChangePhone}
+            style={dynamicStyle}
           />
           <div className={styles.checkboxContainer}>
             <input
@@ -72,7 +83,7 @@ const LoginModal = ({ loginWrapperRef, toggleLoginVisibility }) => {
             className={styles.inputCode}
             placeholder="ХХХХ"
             onChange={handleEnterCode}
-            maxlength="4"
+            maxLength="4"
           />
           <button className={styles.buttonStyle} onClick={handleSendCode}>
             <span className={styles.buttonText}>Выслать код повторно</span>
