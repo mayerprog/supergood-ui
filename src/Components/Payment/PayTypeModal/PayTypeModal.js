143fda3c2b9e918739d3916ecfdfe68ee60002d3
mayerprog <supermayerehs@gmail.com>
2024-04-09 10:56:32 +0300

    feat: add paytype modal for choosing paytype

--- src/Components/Payment/PayTypeModal/PayTypeModal.js
+++ src/Components/Payment/PayTypeModal/PayTypeModal.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import styles from "./PayTypeModal.module.scss";
 
 const PayTypeModal = ({ payTypeWrapperRef }) => {
-  return <div ref={payTypeWrapperRef} className={styles.container}></div>;
+  const [chosenValue, setChosenValue] = useState("");
+
+  const handleCheckBox = (event) => {
+    setChosenValue(event.target.value);
+  };
+
+  return (
+    <div ref={payTypeWrapperRef} className={styles.container}>
+      <h2>Способ оплаты</h2>
+
+      {["Наличными курьеру", "Банковской картой"].map((item, index) => (
+        <div className={styles.payTypeContainer} key={item}>
+          <input
+            type="radio"
+            checked={item === chosenValue}
+            onChange={handleCheckBox}
+            value={item}
+          />
+          <label>{item}</label>
+        </div>
+      ))}
+
+      <div className={styles.buttonsContainer}>
+        <button
+          className={styles.buttonStyle}
+          onClick={() => console.log("Save")}
+        >
+          <span className={styles.buttonText}>Выбрать</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PayTypeModal;
