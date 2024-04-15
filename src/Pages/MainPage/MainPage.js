f150c683aaf37d6d3f058f25d17275700e18405b
mayerprog <supermayerehs@gmail.com>
2024-04-15 11:07:05 +0300

    feat: change headerHeight

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -58,52 +58,52 @@ const MainPage = ({
 
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       setLoading(true);
-
-  //       const itemsArray = [];
-  //       const allItems = await itemAPI.getItems(1257);
-
-  //       Object.keys(allItems.items).forEach((categoryId) => {
-  //         const categoryObject = allItems.items[categoryId];
-  //         const category = Object.values(categoryObject);
-  //         // console.log("category", category);
-  //         category.forEach((itemGroup) => {
-  //           Object.values(itemGroup).forEach((item) => {
-  //             const itemForPush = Object.values(item);
-  //             // console.log("item", itemForPush);
-  //             itemsArray.push(itemForPush[0]);
-  //           });
-  //         });
-  //       });
-  //       dispatch(setItems(itemsArray));
-  //       if (itemsArray.length === 0) setLoading(true);
-  //       else setLoading(false);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   })();
-  // }, [dispatch]);
-
   useEffect(() => {
-    const itemsArray = [];
-    Object.keys(jsonData.items).forEach((categoryId) => {
-      const categoryObject = jsonData.items[categoryId];
-      const category = Object.values(categoryObject);
-      // console.log("category", category);
-      category.forEach((itemGroup) => {
-        Object.values(itemGroup).forEach((item) => {
-          const itemForPush = Object.values(item);
-          itemsArray.push(itemForPush[0]);
+    (async () => {
+      try {
+        setLoading(true);
+
+        const itemsArray = [];
+        const allItems = await itemAPI.getItems(1257);
+
+        Object.keys(allItems.items).forEach((categoryId) => {
+          const categoryObject = allItems.items[categoryId];
+          const category = Object.values(categoryObject);
+          // console.log("category", category);
+          category.forEach((itemGroup) => {
+            Object.values(itemGroup).forEach((item) => {
+              const itemForPush = Object.values(item);
+              // console.log("item", itemForPush);
+              itemsArray.push(itemForPush[0]);
+            });
+          });
         });
-      });
-    });
-    console.log("itemsArray", itemsArray);
-    dispatch(setItems(itemsArray));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        dispatch(setItems(itemsArray));
+        if (itemsArray.length === 0) setLoading(true);
+        else setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
   }, [dispatch]);
+
+  // useEffect(() => {
+  //   const itemsArray = [];
+  //   Object.keys(jsonData.items).forEach((categoryId) => {
+  //     const categoryObject = jsonData.items[categoryId];
+  //     const category = Object.values(categoryObject);
+  //     // console.log("category", category);
+  //     category.forEach((itemGroup) => {
+  //       Object.values(itemGroup).forEach((item) => {
+  //         const itemForPush = Object.values(item);
+  //         itemsArray.push(itemForPush[0]);
+  //       });
+  //     });
+  //   });
+  //   console.log("itemsArray", itemsArray);
+  //   dispatch(setItems(itemsArray));
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [dispatch]);
 
   useEffect(() => {
     let filteredItems = searchQuery.trim()
@@ -130,7 +130,7 @@ const MainPage = ({
 
   useEffect(() => {
     if (headerRef.current) {
-      setHeaderHeight(headerRef.current.offsetHeight); // height of Header
+      setHeaderHeight(headerRef.current.offsetHeight - 5); // height of Header
     }
   }, [headerRef]);
 
--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ import { persistStore } from "redux-persist";
 let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
