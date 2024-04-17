eea9aec99e3b70f298f0ba6d01cce834376055da
mayerprog <supermayerehs@gmail.com>
2024-04-17 14:11:59 +0300

    feat: finish navbar animated scrolling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./NavBar.module.scss";
 
 const NavBar = ({
@@ -16,19 +16,46 @@ const NavBar = ({
   const navbarRef = useRef(null);
   const buttonRefs = useRef([]);
 
+  const [prevActiveButtonId, setPrevActiveButtonId] = useState(0);
+
   useEffect(() => {
-    const activeButton = buttonRefs.current.find(
+    // find index of selected button
+    const activeButtonIndex = buttonRefs.current.findIndex(
       (btn) => btn && btn.textContent === selectedCategory
     );
+    // access the button itself using the index
+    const activeButton = buttonRefs.current[activeButtonIndex];
+
     if (activeButton && navbarRef.current) {
-      const buttonRect = activeButton.getBoundingClientRect();
-      const scrollLeft = buttonRect.left;
-      navbarRef.current.scroll({
-        left: scrollLeft,
-        behavior: "smooth",
-      });
+      //the position of the left edge of the button relative to the left edge of its offset parent
+      const scrollLeft = activeButton.offsetLeft;
+      // when choosing buttons to the right
+      if (activeButtonIndex > prevActiveButtonId) {
+        navbarRef.current.scroll({
+          left: scrollLeft,
+          behavior: "smooth",
+        });
+
+        setPrevActiveButtonId(activeButtonIndex);
+        // when choosing buttons to the left
+      } else if (activeButtonIndex < prevActiveButtonId) {
+        const buttonEndPosition = scrollLeft + activeButton.offsetWidth;
+        const visibleWidth = navbarRef.current.clientWidth;
+
+        navbarRef.current.scroll({
+          left: buttonEndPosition - visibleWidth,
+          behavior: "smooth",
+        });
+        setPrevActiveButtonId(activeButtonIndex);
+      }
+
+      console.log("activeButtonWidth", activeButton.offsetWidth);
+      console.log(
+        "navbarRef.current.clientWidth",
+        navbarRef.current.clientWidth
+      );
     }
-  }, [selectedCategory]);
+  }, [selectedCategory, prevActiveButtonId]);
 
   return (
     <div className={styles.navbar} ref={navbarRef}>
