63c7d2142040e3d7e06c73189d21f42cb1dfca77
mayerprog <supermayerehs@gmail.com>
2024-04-27 16:23:12 +0300

    style: config orderpromomodal

--- src/Components/Promo/OrderPromoModal/OrderPromoModal.js
+++ src/Components/Promo/OrderPromoModal/OrderPromoModal.js
@@ -11,12 +11,29 @@ const OrderPromoModal = ({
         <IoMdClose size={25} />
       </div>
       <div>
-        <h3>Активируйте промокод</h3>
-        <input placeholder="Введите промокод" className={styles.input} />
-      </div>
-      <div>
-        <h3>Используйте бонусы</h3>
-        <input className={styles.input} />
+        <div>
+          <h3>Активируйте промокод</h3>
+          <input placeholder="Введите промокод" className={styles.input} />
+        </div>
+        <button className={styles.buttonStyle}>
+          <span className={styles.buttonText}>Активировать</span>
+        </button>
+        <div>
+          <h3>Используйте бонусы</h3>
+          <div className={styles.info}>
+            Доступно бонусов: <span>2000</span>
+          </div>
+          <div className={styles.info}>
+            Списать бонусов: <span>180</span>
+          </div>
+          <input placeholder="Введите количество" className={styles.input} />
+          <div className={styles.addInfo}>
+            Оплата не более 30% от стоимости заказа
+          </div>
+        </div>
+        <button className={styles.buttonStyle}>
+          <span className={styles.buttonText}>Применить</span>
+        </button>
       </div>
     </div>
   );
